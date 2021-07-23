f04dce6f694d32e56909a3c1b668ab7896a7ac71
Oleksiy Potanin <potashca98@gmail.com>
2021-07-23 09:13:33 +0300

    create, view, add, delete, edit posts

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class WikiPage(models.Model):
     title = models.CharField(verbose_name='Заголовок', max_length=250)
     content = models.TextField(verbose_name='Контент', )
-    current_version = models.PositiveIntegerField(verbose_name='Поточна версія', unique=True)
+    current_version = models.PositiveIntegerField(verbose_name='Поточна версія',)
 
     def __str__(self):
         return self.title
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import WikiPage
+from .forms import CreateNewPage
 
 
 def index(request):
@@ -12,4 +13,53 @@ def index(request):
 
 
 def add_new_record(request):
-    return render(request, 'add_new.html')
+    if request.method == 'POST':
+        form = CreateNewPage(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            WikiPage.objects.create(title=data['title'],
+                                    content=data['content'],
+                                    current_version=14)
+        return redirect('home')
+    else:
+        form = CreateNewPage()
+        return render(request, 'add_new.html',
+                      {
+                          'form': form
+                      }
+                      )
+
+
+def view_record(request, record_id):
+    record = WikiPage.objects.get(pk=record_id)
+    content = {
+        'title': record.title,
+        'record': record,
+    }
+    return render(request, 'record.html', content)
+
+
+def edit_record(request, record_id):
+    if request.method == 'POST':
+        record = WikiPage.objects.get(pk=record_id)
+        record.title = request.POST.get("title")
+        record.content = request.POST.get("content")
+        record.save()
+        content = {
+            'title': f'Редагування сторінки {record_id}',
+            'record': record,
+        }
+        return render(request, 'record.html', content)
+    else:
+        record = WikiPage.objects.get(pk=record_id)
+        content = {
+            'title': f'Редагування сторінки {record_id}',
+            'record': record,
+        }
+        return render(request, 'edit.html', content)
+
+
+def delete_record(request, record_id):
+    record = WikiPage.objects.get(pk=record_id)
+    record.delete()
+    return redirect('home')
